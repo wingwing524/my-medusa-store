a0a2bcfa373773cb9064b1f02136a00b1e22c101
IsaacYam <howingyam0350@gmail.com>
2025-11-25 23:47:00 +0800

    update

---
+++ my-medusa-store/medusa-config.ts
@@ -0,0 +1,30 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  modules: [
+    {
+      resolve: "./src/modules/page",
+      options: {},
+    },
+    {
+      resolve: "./src/modules/promotional-banner",
+      options: {},
+    },
+    {
+      resolve: "./src/modules/favorite",
+      options: {},
+    },
+  ],
+})
